960dad3408ca4658ac8b34a5b658fdb905626da0
Carlos Gonçalves <cgoncalves@av.it.pt>
2013-02-18 10:25:12 +0000

    Create new user method

--- lib/csb/user.rb
+++ lib/csb/user.rb
@@ -1,7 +1,16 @@
 module Csb
   class Client
-    def user_info
+    def user
       get('/users.json')
+    end
+    alias user_info user
+
+    # Valid parameters are:
+    #   :name      Unique User name
+    #   :email     Email address
+    #   :password  Password
+    def user_new(params)
+      post('/users.json', params)
     end
   end
 end
